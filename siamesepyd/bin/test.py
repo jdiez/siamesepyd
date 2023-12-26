1fa11282c773de2a10c11fe6e94ae15cc56fe722
Javier Diez Perez <javier.diez@astrazeneca.com>
2023-12-26 21:24:28 +0100

    cli added to test

--- siamesepyd/bin/test.py
+++ siamesepyd/bin/test.py
@@ -1,16 +1,31 @@
 """To test entry points later on.
 """
 
+from typing import Annotated
+
+import typer
+
 from siamesepyd.core.siamese import MyUuidMetadataBaseModel, SiameseUUID
 
 
-def main():
-    Siamese = SiameseUUID(key_seed="ENABL")  # This could be replaced with project uuid.
-    print("Proper UUID (uuid5): ", Siamese(MyUuidMetadataBaseModel(salt="LUNG342")))
-    print("Validated 'LUNG342', 'ABGZMV': ", Siamese.validate_keys("LUNG342", "ABGZMV"))
-    print("Validated 'LUNG342', 'ABGZMY': ", Siamese.validate_keys("LUNG342", "ABGZMY"))
-    print("Validated 'LUNG341', 'ABGZMV': ", Siamese.validate_keys("LUNG341", "ABGZMV"))
+def main(
+    input_seed: Annotated[str, typer.Option(help="Table format.")] = "ENABL",
+    input_salt: Annotated[str, typer.Option(help="Table format.")] = "LUNG342",
+    expected_siamese: Annotated[str, typer.Option(help="Table format.")] = "ABGZMV",
+):
+    """AI is creating summary for main
+
+    Args:
+        input_seed (Annotated[str, typer.Option, optional): [description]. Defaults to "Table format.")]="ENABL".
+        input_salt (Annotated[str, typer.Option, optional): [description]. Defaults to "Table format.")]="LUNG342".
+        expected_siamese (Annotated[str, typer.Option, optional): [description]. Defaults to "Table format.")]="ABGZMV".
+    """
+    Siamese = SiameseUUID(key_seed=input_seed)  # This could be replaced with project uuid.
+    print("Proper UUID (uuid5): ", Siamese(MyUuidMetadataBaseModel(salt=input_salt)))
+    print(f"Validated '{input_salt}', '{expected_siamese}': ", Siamese.validate_keys(input_salt, expected_siamese))
+    print(f"Validated '{input_salt}', 'ABGZMY': ", Siamese.validate_keys(input_salt, "ABGZMY"))
+    print(f"Validated 'LUNG341', '{expected_siamese}': ", Siamese.validate_keys("LUNG341", expected_siamese))
 
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
