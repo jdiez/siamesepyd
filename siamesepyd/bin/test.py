e9b2efb47579fb34ba0c3cd27f822897f4765929
Javier Diez Perez <javier.diez@astrazeneca.com>
2023-12-26 21:29:35 +0100

    cli added to test

--- siamesepyd/bin/test.py
+++ siamesepyd/bin/test.py
@@ -9,22 +9,16 @@ from siamesepyd.core.siamese import MyUuidMetadataBaseModel, SiameseUUID
 
 
 def main(
-    input_seed: Annotated[str, typer.Option(help="Table format.")] = "ENABL",
-    input_salt: Annotated[str, typer.Option(help="Table format.")] = "LUNG342",
-    expected_siamese: Annotated[str, typer.Option(help="Table format.")] = "ABGZMV",
+    salt: Annotated[str, typer.Option(help="Table format.")] = "ENABL",
+    left: Annotated[str, typer.Option(help="Table format.")] = "LUNG342",
+    right: Annotated[str, typer.Option(help="Table format.")] = "ABGZMV",
 ):
-    """AI is creating summary for main
-
-    Args:
-        input_seed (Annotated[str, typer.Option, optional): [description]. Defaults to "Table format.")]="ENABL".
-        input_salt (Annotated[str, typer.Option, optional): [description]. Defaults to "Table format.")]="LUNG342".
-        expected_siamese (Annotated[str, typer.Option, optional): [description]. Defaults to "Table format.")]="ABGZMV".
-    """
-    Siamese = SiameseUUID(key_seed=input_seed)  # This could be replaced with project uuid.
-    print("Proper UUID (uuid5): ", Siamese(MyUuidMetadataBaseModel(salt=input_salt)))
-    print(f"Validated '{input_salt}', '{expected_siamese}': ", Siamese.validate_keys(input_salt, expected_siamese))
-    print(f"Validated '{input_salt}', 'ABGZMY': ", Siamese.validate_keys(input_salt, "ABGZMY"))
-    print(f"Validated 'LUNG341', '{expected_siamese}': ", Siamese.validate_keys("LUNG341", expected_siamese))
+    """AI is creating summary for main."""
+    Siamese = SiameseUUID(key_seed=salt)  # This could be replaced with project uuid.
+    print("Proper UUID (uuid5): ", Siamese(MyUuidMetadataBaseModel(salt=left)))
+    print(f"Validated '{left}', '{right}': ", Siamese.validate_keys(left, right))
+    print(f"Validated '{left}', 'ABGZMY': ", Siamese.validate_keys(left, "ABGZMY"))
+    print(f"Validated 'LUNG341', '{right}': ", Siamese.validate_keys("LUNG341", right))
 
 
 if __name__ == "__main__":
